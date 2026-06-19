c3b2525180f020ee04f9e5c09e6fe4170bc24835
amLarz <larzroco@gmail.com>
2026-06-19 19:41:09 +0800

    Changed Structure to make main a function
    made the render for each screen a function in order to be able to press buttons and having main take care of the gameloop

--- Game/PygameVersion/main.py
+++
@@ -1,7 +0,0 @@
-import pygame
-import sys
-
-# Setup 
-currentState = "menu"
-
-# TODO: if play is pressed in menu current state turns into game
---
+++ Game/PygameVersion/states/main.py
@@ -0,0 +1,16 @@
+import pygame
+import sys
+from menu import run_menu
+from game import run_game
+
+# Setup 
+currentState = run_menu()
+
+# Game loop checks for the state of the game 
+while True:
+    if currentState == "menu":
+        run_menu()
+    
+    if currentState == "game":
+        run_game()
+    
