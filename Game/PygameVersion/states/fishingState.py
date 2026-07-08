7123adf5a3188d89fb70641aafce8ebd428e20a7
amLarz <larzroco@gmail.com>
2026-07-08 16:00:12 +0800

    the start of redoing the fishing to actually fit gameplay rather tahn just a prototype

--- Game/PygameVersion/states/fishingState.py
+++ Game/PygameVersion/states/fishingState.py
@@ -1,5 +1,6 @@
 import random
 import csv 
+import pygame
 
 # Depth switcher
 def permitted_depth():
@@ -12,8 +13,31 @@ def read_fishes():
         for row in fishreader:
             if 
 
-# SHALLOW LEVEL FISH 
-shallow_fish = ['Clown']
+
+def waitFish():
+
+    # Set up for time 
+    shallow_catch_time = random.randint(3000, 10000)
+    time_start = pygame.time.get_ticks()
+    time_end = time_start + shallow_catch_time
+
+    last_tick = time_start # every second start
+    current_time = time_start # ever milisecond start
+
+    # Keeps space for adding dots
+    print("fishing", end="")
+
+    while current_time <= time_end:
+        
+        current_time = pygame.time.get_ticks()
+
+        if last_tick <= current_time - 1000:
+            last_tick += 1000
+            print(".", end="")
+
+    print("")
+    catchRNG()
+    return 
 
 def catchRNG():
     shallowFish = ["Minnow", "Shark", "><))*>"]
--- Game/PygameVersion/states/game.py
+++ Game/PygameVersion/states/game.py
@@ -2,7 +2,6 @@
 import pygame
 import random
 import time
-from fishingState import catchRNG
 
 
 
@@ -12,9 +11,9 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 player_state = False # made to be global
 
-def callState():
-    global player_state
-    return player_state
+# depth
+depth = ['shallow-water', 'mid-water', 'deepwater']
+
 
 def switchState():
     global player_state
@@ -27,50 +26,18 @@ def switchState():
         current_state = True
 
     player_state = current_state
-    stateMachine()  
-
-# Changes State from idle to fishing
-def stateMachine():
-
-    fishing_state = callState()
     
-    # Proceed to wait fish
-    if fishing_state == True:
-        waitFish()
-    else:
-        pass
-
-def waitFish():
-
-    # Set up for time 
-    global player_state
-    shallow_catch_time = random.randint(3000, 10000)
-    time_start = pygame.time.get_ticks()
-    time_end = time_start + shallow_catch_time
-
-    last_tick = time_start # every second start
-    current_time = time_start # ever milisecond start
-
-    # Keeps space for adding dots
-    print("fishing", end="")
-
-    while current_time <= time_end:
-        
-        current_time = pygame.time.get_ticks()
-
-        if last_tick <= current_time - 1000:
-            last_tick += 1000
-            print(".", end="")
-
-    print("")
-    catchRNG()
-    return 
-    
+    if player_state == True:
+        # move to fishingState.py
+        return
 
 # running and rendering the game 
 def run_game():
     # setup and render
     running = True
+    selection = 0
+    
+
     while running:
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
@@ -83,6 +50,16 @@ def run_game():
 
                 if event.key == pygame.K_x:
                     switchState()
+
+                if event.key == pygame.K_DOWN and selection != 2:
+                    selection += 1
+                
+                if event.key == pygame.K_UP and selection != 0:
+                    selection -= 1
+
+        # selection
+        print(depth[selection])
+            
 
         # fill the screen with a color to wipe away anything from last frame
         screen.fill("purple")
