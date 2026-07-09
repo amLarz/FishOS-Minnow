d7ed124df4adc4fb01235b3d8aed0938c00e29bd
amLarz <larzroco@gmail.com>
2026-07-09 22:44:21 +0800

    cleaned some not needed comments

--- Game/PygameVersion/states/fishingState.py
+++ Game/PygameVersion/states/fishingState.py
@@ -14,7 +14,7 @@ def load_fishes(depth_scope):
 
     with open(FISH_CSV_PATH, newline='') as csvfile:
         fishreader = csv.DictReader(csvfile)
-        for row in fishreader: # WORK ON THIS ITS ONLY PUTTING BASED ON THE LITERAL LOOP NOT THE DEPTH
+        for row in fishreader: 
             if row['depth'] in depth_scope:
                 possible_fishes.append(row)
 
@@ -23,11 +23,11 @@ def load_fishes(depth_scope):
 def waitFish():
 
     # Set up for time 
-    shallow_catch_time = random.randint(3000, 10000)
-    time_start = pygame.time.get_ticks()
-    time_end = time_start + shallow_catch_time
+    catch_time = random.randint(3000, 10000)
+    time_start = pygame.time.get_ticks() 
+    time_end = time_start + catch_time
 
-    last_tick = time_start # every second start
+    last_tick = time_start # every second start 
     current_time = time_start # ever milisecond start
 
     # Keeps space for adding dots
@@ -42,6 +42,8 @@ def waitFish():
             print(".", end="")
 
     print("")
+
+    return 
 
 #TODO: FINISH CATCHING RNG SYSTEM
 def catchRNG(fishes, depth_scope):
